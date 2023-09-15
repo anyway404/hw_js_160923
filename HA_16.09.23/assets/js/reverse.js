191e943a4476a15525f94d6c56d002a18bfe68e8
anyway404 <106810071+anyway404@users.noreply.github.com>
2023-09-15 18:04:34 +0200

    Add files via upload

---
+++ HA_16.09.23/assets/js/reverse.js
@@ -0,0 +1,11 @@
+function reverseString(str) {
+  let splitString = str.split("");
+  console.log(splitString);
+
+
+  let joinArray = reverseArray.join("");
+
+  return joinArray;
+}
+
+reverseString("hello");
